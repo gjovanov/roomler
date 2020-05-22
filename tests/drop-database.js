78b4866aa64b225116c125f1e402f05e45b1790a
Goran Jovanov <goran.jovanov@gmail.com>
2020-05-22 11:20:21 +0300

    fix in drop DB used by tests

--- tests/drop-database.js
+++ tests/drop-database.js
@@ -1,8 +1,13 @@
 (async function () {
   try {
     const consola = require('consola')
-    const fastify = require('../api/api')()
     consola.info('Starting Fastify server for DB cleanup...')
+    const fastify = require('fastify')({
+      logger: false
+    })
+      .register(
+        require('../api/plugins/mongoose/fastify-mongoose'),
+        require('../api/plugins/mongoose/mongoose-options'))
     await fastify.ready()
     consola.info('Dropping DB...')
     await fastify.mongo.db.dropDatabase()
