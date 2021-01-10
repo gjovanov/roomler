cdf32ff5ebc4b5f27ea7759ea7aa6b23bed4c0e9
gjovanov <goran.jovanov@gmail.com>
2021-01-10 10:07:42 +0100

    version 0.4.3

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -2,4 +2,4 @@ set -ex
 # SET THE FOLLOWING VARIABLES
 USERNAME=gjovanov
 IMAGE=roomler
-docker build --network=host -t $USERNAME/$IMAGE:latest $(for i in `cat .arg`; do out+="--build-arg $i " ; done; echo $out;out="") ../
+docker build --network=host -t $USERNAME/$IMAGE:latest $(for i in `cat .arg`; do out+="--build-arg $i " ; done; echo $out;out="") .
